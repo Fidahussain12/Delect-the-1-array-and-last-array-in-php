d89180f9d29e9bcc5bea8341be65494d1cb82665
Hussain_ali <165026313+Fidahussain12@users.noreply.github.com>
2024-05-07 12:43:48 -0700

    Add files via upload

---
+++ delect 1 and last arary.php
@@ -0,0 +1,15 @@
+<pre>
+<?php
+// Example array
+$myArray = [10, 20, 30, 40, 50];
+
+// Remove the first element
+array_shift($myArray);
+
+// Remove the last element
+array_pop($myArray);
+
+// Print the modified array
+print_r($myArray);
+?>
+</pre>
